8f36928b2de0525fdce822968c41cf016c907e66
Ivan <ivan129522@gmail.com>
2020-03-09 17:50:08 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.nav-btn').onclick = function() {
+  document.querySelector('.nav').classList.toggle('nav--active');
+};
+
+document.querySelector('.nav-btn.close-btn').onclick = function() {
+  document.querySelector('.nav').classList.toggle('nav--active');
+};
